7deb853bc130466b6958c7ad4c6667acdec80005
Dharshh1805 <108127314+Dharshh1805@users.noreply.github.com>
2024-04-14 18:08:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ mongoose.set('strictQuery', true);
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use('*', cors({
+    origin:process.env.ORIGIN,
+    credentials : true,
+    methods : ['GET','POST','UPDATE','DELETE']
+}));
 
 //db config
 mongoose.connect(process.env.MONGO_URI, {
